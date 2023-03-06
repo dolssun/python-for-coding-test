873c44ca457abf50f55c6bd01f8e1d3426d8f0b9
dolssun <903ejkim@naver.com>
2023-03-06 10:15:14 +0900

    [백준11399] SORT : ATM - 누적 합 코드 변경

--- Chater6/codetest/230306_2.py
+++ Chater6/codetest/230306_2.py
@@ -4,14 +4,16 @@ n = int(input())
 array = list(map(int, input().split()))
 
 array.sort()
-sum = 0
+total = 0
 
-for i in range(n):
-    for j in range(0,i+1,1):
-        #print(array[j], end=' ')
-        sum += array[j]
+#for i in range(n):
+ #   for j in range(0,i+1,1):
+  #      #print(array[j], end=' ')
+   #     sum += array[j]
 
-print(sum)
+for i in range(1, n+1):
+    total += sum(array[0:i])
+print(total)
 
 
 
