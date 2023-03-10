9cbfb94af36aed7d2230edf0c2b0181d866394b4
dolssun <903ejkim@naver.com>
2023-03-10 12:07:15 +0900

    [백준18870]SORT : 좌표 압축 _ 코드수정

--- Chater6/codetest/bj_18870.py
+++ Chater6/codetest/bj_18870.py
@@ -1,18 +1,30 @@
 # 백준 18870 : 좌표 압축
-import sys
 
+import sys
 input = sys.stdin.readline
+
 n = int(input())
 array = list(map(int, input().split()))
 
-print(array)
-
 sorted_list = sorted(list(set(array)))
-print(sorted_list)
 
 dic = {sorted_list[i]:i for i in range(len(sorted_list))}
-print(dic)
+
 for i in array:
     print(dic[i], end=' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
